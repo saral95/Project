471b3e28aa0f8e81f7b92ec61f03543a66ed0e4c
saral95 <sarakennedy0611@gmail.com>
2021-01-08 15:46:01 +0530

    project

---
+++ src/org/test/CollegeInformation.java
@@ -0,0 +1,19 @@
+package org.test;
+
+
+
+public class CollegeInformation{
+
+		public void collegeName() {
+			System.out.println("College Name: Kingston Engineering College");
+
+		}
+		public void collegeCode() {
+			System.out.println("College Code: 1845");
+
+		}
+		public void collegeRank() {
+			System.out.println("College Rank: 2");
+
+}
+}
